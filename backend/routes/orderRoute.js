ecfc7251c526816b295ca02af1271763da49622c
Mustu120 <143619621+Mustu120@users.noreply.github.com>
2025-09-21 13:04:06 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,9 +3,7 @@ import {
     placeOrder, 
     userOrders, 
     allOrders, 
-    updateStatus,
-    createRazorpayOrder,
-    verifyPayment
+    updateStatus
 } from '../controllers/orderController.js';
 import authUser from '../middleware/auth.js';
 import adminAuth from '../middleware/adminAuth.js';
@@ -15,10 +13,6 @@ const orderRouter = express.Router();
 // User Routes
 orderRouter.post('/place', authUser, placeOrder); // For COD
 orderRouter.get('/userorders', authUser, userOrders);
-
-// Razorpay Routes
-orderRouter.post('/create-razorpay-order', authUser, createRazorpayOrder);
-orderRouter.post('/verify-payment', authUser, verifyPayment);
 
 // Admin Routes
 orderRouter.get('/list', authUser, adminAuth, allOrders);
